58cf242f08197c0946abf8f98e8593311b81eba4
vffs <vffs@a.recife.ifpe.edu.br>
2021-01-22 19:45:48 -0300

    clicando mais de uma vez no botao igual

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,9 @@ class CalcController{
 
     constructor(){
 
+        this._lastOperator = '';
+        this._lastNumber = '';
+
         this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector('#display');
@@ -56,15 +59,30 @@ class CalcController{
        return ['+', '-', '*', '%', '/'].indexOf(value) > -1 ;
     }
 
+    getResult(){
+        return eval(this._operation.join(''));
+    }
+
     calc(){
 
         let last = '';
+        this._lastOperator = this.getLastItem();
+
+        if(this._operation.length < 3){
+            let firstItem = this._operation[0] ;
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
+        }
 
         if(this._operation.length > 3){
             last = this._operation.pop();
+            this._lastNumber = this.getResult();
+        }
+        else if(this._operation.length == 3){
+
+            this._lastNumber = this.getLastItem(false);
         }
      
-        let result = eval(this._operation.join(''));
+        let result = this.getResult();
 
         if(last == '%'){
             result /= 100;
@@ -91,17 +109,30 @@ class CalcController{
         }
 
     }
+    
+    getLastItem(isOperator = true){
+
+        let lastItem;
+        
+        for(let i = this._operation.length - 1; i >= 0 ; i--){
+            
+                if(this.isOperator(this._operation[i]) == isOperator){
+                    lastItem = this._operation[i];
+                    break;
+                }
+            
+        }
+
+        if(!lastItem){
+            lastItem = (isOperator)? this._lastOperator : this.lastNumber;
+        }
+
+        return lastItem;
+    }
 
     setLastNumberToDisplay(){
         
-        let lastNumber;
-        
-        for(let i = this._operation.length - 1; i >= 0 ; i--){
-            if(!this.isOperator(this._operation[i])){
-                lastNumber = this._operation[i];
-                break;
-            }
-        }
+        let lastNumber = this.getLastItem(false);
 
         if(!lastNumber) lastNumber = 0;
 
@@ -109,8 +140,6 @@ class CalcController{
     }
 
     addOperation(value){
-
-      //  console.log('A' , value,  isNaN(this.getLastOperation()));
 
         if(isNaN(this.getLastOperation())){
 
