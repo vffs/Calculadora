7f2ebc257ec0897bab0de2b2bebaf2a1874b1442
vffs <vffs@a.recife.ifpe.edu.br>
2021-01-29 12:17:29 -0300

    inserindo audio

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,7 +1,9 @@
 class CalcController{
 
     constructor(){
-
+ 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
 
@@ -49,11 +51,33 @@ class CalcController{
 
      this.setLastNumberToDisplay();
      this.pasteFromClipboard();
+
+     document.querySelectorAll('.btn-ac').forEach(btn => {
+        btn.addEventListener('dblclick', e => {
+            this.toggleAudio();
+        }); 
+     });
      
+    }
+
+    toggleAudio(){
+
+        this._audioOnOff = !this._audioOnOff;
+       
+    }
+
+    playAudio(){
+
+        if(this._audioOnOff){
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     initKeyBoard(){
         document.addEventListener('keyup', e => {
+
+            this.playAudio();
            
             switch(e.key){
 
@@ -273,6 +297,8 @@ class CalcController{
     }
   
      execBtn(value){
+
+        this.playAudio();
 
         switch(value){
 
