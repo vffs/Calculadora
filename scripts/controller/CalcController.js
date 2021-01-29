945cfc8df0ece1a07794dea14ab92d79b7b708ce
vffs <vffs@a.recife.ifpe.edu.br>
2021-01-29 12:38:52 -0300

    tratar outros erros com try catch

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -164,7 +164,14 @@ class CalcController{
     }
 
     getResult(){
+        try{
         return eval(this._operation.join(''));
+        }
+        catch(e){
+            setTimeout(()=> {
+                this.setError();
+            },1);
+        }
     }
 
     calc(){
